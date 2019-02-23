8a2bdd515377239dcb335fe54de00590bee44672
hug0er <hugo.e.betancourt@gmail.com>
2019-02-23 15:36:59 -0500

    ventana

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WindowComponent } from './window/window.component';
+import { DataComponent } from './data/data.component';
+import { PoolComponent } from './pool/pool.component';
+import { MetaLearningComponent } from './meta-learning/meta-learning.component';
+import { HyperparametersComponent } from './hyperparameters/hyperparameters.component';
+import { SummaryComponent } from './summary/summary.component';
+import { BarComponent } from './bar/bar.component';
+import { GraphicsComponent } from './graphics/graphics.component';
+import { RunLearningComponent } from './run-learning/run-learning.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [{ path: 'Bar', component: BarComponent },
+{ path: 'Graphics', component: GraphicsComponent },
+{ path: 'RunLearning', component: RunLearningComponent },
+{ path: 'Summary', component: SummaryComponent },
+{ path: 'Hyperparameters', component: HyperparametersComponent },
+{ path: 'MetaLearning', component: MetaLearningComponent },
+{ path: 'Pool', component: PoolComponent },
+{ path: 'Window', component: WindowComponent },
+{ path: 'Data', component: DataComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
