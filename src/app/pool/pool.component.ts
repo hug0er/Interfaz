a501bfe7a8349997663d97a623b1c26b36e4ca8f
DonHachi <carlos.britog25@gmail.com>
2019-02-25 19:27:29 -0500

    pools template added, may e used for metalearning page as well

--- src/app/pool/pool.component.ts
+++ src/app/pool/pool.component.ts
@@ -5,11 +5,23 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './pool.component.html',
   styleUrls: ['./pool.component.scss']
 })
-export class PoolComponent implements OnInit {
 
-  constructor() { }
+export class PoolComponent implements OnInit {
+  public methods = {
+    "method1":false,
+    "method2":false,
+    "method3":false,
+    "method4":false
+  };
+  constructor() { 
+    
+  }
 
   ngOnInit() {
+  }
+  chooseMethod(method){
+    this.methods[method]= !this.methods[method];
+    console.log(this.methods);
   }
 
 }
